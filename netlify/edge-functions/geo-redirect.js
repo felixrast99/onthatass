143e50dd3eb17e85abe196f731cc8b2c4595033a
felixrast99 <felixrast99@gmail.com>
2026-05-14 02:24:30 +0100

    Update geo-redirect.js

--- netlify/edge-functions/geo-redirect.js
+++ netlify/edge-functions/geo-redirect.js
@@ -3,7 +3,8 @@ export default async (request, context) => {
   const country = context.geo?.country?.code || "UNKNOWN";
 
   const geoLinks = {
-    "FR": "https://onthatass.com/fr-fr/men/signup?tid=&utm_source=everflow&utm_campaign=uk&utm_medium=cpa&oid=212&affid=464"
+    "FR": "https://onthatass.com/fr-fr/men/signup?tid=&utm_source=everflow&utm_campaign=uk&utm_medium=cpa&oid=212&affid=464",
+    "DE": "https://www.onthatass-deals.com/Q2S9MT/C2WBD8/"
   };
 
   if (geoLinks[country]) {
